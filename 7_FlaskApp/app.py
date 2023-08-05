8e80ba791ecf5505f171d38bd4227ee364175111
MuhammadZainAmmad <zainammad05@gmail.com>
2023-08-05 22:52:54 +0500

    code added to store the predicted image along with its label in the db

--- 7_FlaskApp/app.py
+++ 7_FlaskApp/app.py
@@ -3,10 +3,22 @@ import numpy as np
 import tensorflow as tf
 from flask import Flask, render_template, request, jsonify
 from PIL import Image
-# import my_model  # Renamed model.py to my_model.py
+import mysql.connector
 
 app = Flask(__name__)
 model_path = './ANN_2Hidden.h5'
+
+# MySQL database configuration
+db_config = {
+    'host': 'localhost',
+    'user': 'root',
+    'password': 'root',
+    'database': 'capture_dress',
+}
+
+# Connect to the MySQL database
+connection = mysql.connector.connect(**db_config)
+cursor = connection.cursor()
 
 def preprocess_image(image):
     # Resize and preprocess the image before making predictions
@@ -37,6 +49,31 @@ def predict():
 
         # Return the predicted label as a JSON response
         return jsonify({'predicted_label': predicted_label})
+    else:
+        return jsonify({'error': 'No file uploaded.'}), 400
+
+@app.route('/store_image', methods=['POST'])
+def store_image():
+    file = request.files['file']
+    if file:
+        img = Image.open(file)
+        input_img = preprocess_image(img)
+
+        # Load the model and make predictions
+        loaded_model = tf.keras.models.load_model(model_path)
+        prediction = loaded_model.predict(input_img)
+        predicted_label = np.argmax(prediction)
+
+        # Convert predicted_label to a Python int
+        predicted_label = int(predicted_label)
+
+        # Save the image and label in the database
+        img_byte_array = file.read()
+        insert_query = "INSERT INTO predicted_imgs (img, label) VALUES (%s, %s)"
+        cursor.execute(insert_query, (img_byte_array, predicted_label))
+        connection.commit()
+
+        return jsonify({'message': 'Image and label stored successfully!'})
     else:
         return jsonify({'error': 'No file uploaded.'}), 400
 
--- 7_FlaskApp/static/script.js
+++ 7_FlaskApp/static/script.js
@@ -1,5 +1,6 @@
 const fileUpload = document.getElementById('file-upload');
 const predictButton = document.getElementById('predict-button');
+const storeButton = document.getElementById('store-button');
 const previewImage = document.getElementById('preview-image');
 const resultDiv = document.getElementById('result');
 
@@ -18,18 +19,63 @@ fileUpload.addEventListener('change', function () {
 
         reader.readAsDataURL(fileUpload.files[0]);
         predictButton.disabled = false;
+        storeButton.disabled = false;
     }
 });
 
-document.getElementById('predict-button').addEventListener('click', async () => {
-    const formData = new FormData();
-    formData.append('file', fileUpload.files[0]);
-    const response = await fetch('/predict', {
-        method: 'POST',
-        body: formData
-    });
-    const data = await response.json();
+predictButton.addEventListener('click', async () => {
+    if (fileUpload.files && fileUpload.files[0]) {
+        const formData = new FormData();
+        formData.append('file', fileUpload.files[0]);
 
-    const predictedLabel = labelMap[data.predicted_label];
-    resultDiv.innerText = `Predicted Label: ${predictedLabel}`;
+        const response = await fetch('/predict', {
+            method: 'POST',
+            body: formData
+        });
+
+        if (response.ok) {
+            const data = await response.json();
+            const predictedLabel = labelMap[data.predicted_label];
+            resultDiv.innerText = `Predicted Label: ${predictedLabel}`;
+        } else {
+            resultDiv.innerText = 'Failed to make a prediction.';
+        }
+    } else {
+        resultDiv.innerText = 'Please choose an image first.';
+    }
+});
+
+storeButton.addEventListener('click', async () => {
+    if (fileUpload.files && fileUpload.files[0]) {
+        const formData = new FormData();
+        formData.append('file', fileUpload.files[0]);
+
+        const predictResponse = await fetch('/predict', {
+            method: 'POST',
+            body: formData
+        });
+
+        if (predictResponse.ok) {
+            const predictData = await predictResponse.json();
+            const predictedLabel = labelMap[predictData.predicted_label];
+
+            // Send both the image and predicted label together
+            formData.append('predicted_label', predictedLabel);
+
+            const storeResponse = await fetch('/store_image', {
+                method: 'POST',
+                body: formData
+            });
+
+            if (storeResponse.ok) {
+                alert('Image and label stored successfully!');
+            } else {
+                alert('Failed to store image and label.');
+            }
+        } else {
+            alert('Failed to make a prediction.');
+        }
+    } else {
+        alert('Please choose an image first.');
+    }
 });
